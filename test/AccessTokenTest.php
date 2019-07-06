5cb8028812af690d070f3e97227e3ac82773334a
wangmin <stlswm@163.com>
2019-07-06 10:02:11 +0800

    up

--- test/AccessTokenTest.php
+++ test/AccessTokenTest.php
@@ -19,7 +19,7 @@ class AccessTokenTest extends TestCase
      */
     public function testReq()
     {
-        $client = Client::newClient('47jJn7w4Kc6oHl', 'UmuZA9Rf7366KQDa84a72cg3x8r9c7', '7h6BNaX0I7mZkAWG');
+        $client = Client::newClient('', '', '');
         $client->asTest();
         $req = new AccessTokenReq();
         $req->client_id = $client->clientId;
--- test/InvoiceCreateTest.php
+++ test/InvoiceCreateTest.php
@@ -18,7 +18,7 @@ class InvoiceCreateTest extends TestCase
      */
     public function testReq()
     {
-        $client = Client::newClient('47jJn7w4Kc6oHl', 'UmuZA9Rf7366KQDa84a72cg3x8r9c7', '7h6BNaX0I7mZkAWG');
+        $client = Client::newClient('', '', '');
         $client->asTest();
         $req = new InvoiceCreateReq();
         var_dump(InvoiceCreate::req($client, '4968422f42fb6554943c2f7195c1eea4', $req));
