5949df88d318ffdb229a6fd8fea6f698ebe9a041
wangmin <stlswm@163.com>
2019-07-08 17:25:55 +0800

    fix type bug

--- src/Response/InvoiceCreateRes.php
+++ src/Response/InvoiceCreateRes.php
@@ -20,9 +20,7 @@ class InvoiceCreateRes extends ClassMap
     public function getClassMap(): array
     {
         return [
-            'data' => [
-                InvoiceCreateDataRes::class,
-            ]
+            'data' => InvoiceCreateDataRes::class,
         ];
     }
 
