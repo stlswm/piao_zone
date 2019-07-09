b09d5e30e8fd3ec659d2f7ee8bc8ede666af89a4
wangmin <stlswm@163.com>
2019-07-09 10:42:02 +0800

    支持发票查询接口

--- test/InvoiceQueryTest.php
+++ test/InvoiceQueryTest.php
@@ -18,11 +18,11 @@ class InvoiceQueryTest extends TestCase
      */
     public function testReq()
     {
-        $client = Client::newClient('47jJn7w4Kc6oHl', 'UmuZA9Rf7366KQDa84a72cg3x8r9c7', '7h6BNaX0I7mZkAWG');
+        $client = Client::newClient('', '', '');
         $client->asTest();
-        $client->setToken('119aaa3796f7fc9d852ad158d0610a66');
+        $client->setToken('');
         $req = new InvoiceQueryReq();
-        $req->serialNo = '20190708170135892499';
+        $req->serialNo = '';
         var_dump(InvoiceQuery::req($client, $req));
     }
 }
